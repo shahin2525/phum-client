026d0baa9c9f0ebb57d8af9a15a8d62b7467989d
shahin2525 <shahin451999@gmail.com>
2025-01-12 19:41:52 -0800

    add persist

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,29 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
-import {
-  useLoginMutation,
-  useUserLoginMutation,
-} from "../redux/feature/auth/authApi";
-
+import { useLoginMutation } from "../redux/feature/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/feature/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+type UserData = {
+  id: string;
+  password: string;
+};
 const Login = () => {
-  const [login, { data, error }] = useLoginMutation();
-  console.log("data=>", data);
-  console.log("error=>", error);
+  const dispatch = useAppDispatch();
+  const [login] = useLoginMutation();
+  // console.log("data=>", data);
+  // console.log("error=>", error);
 
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const onSubmit = async (data: UserData) => {
     const userInfo = {
       id: data.id,
       password: data.password,
     };
-    login(userInfo);
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+    console.log(res);
   };
   return (
     <div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../redux/feature/auth/authSlice.ts";
 import { baseApi } from "./api/baseApi.ts";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "auth",
+
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, authSlice);
 export const store = configureStore({
   reducer: {
-    auth: authSlice,
+    auth: persistedReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
