c186a1663bef9b86ed096ca9efc409b3ea02ef20
shahin2525 <shahin451999@gmail.com>
2025-01-21 12:39:41 -0800

    add semestr year

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -7,6 +7,7 @@ import PHSelect from "../../../components/form/PHSelect";
 'Autumn',
   'Summer',
   'Fall',
+  ['01', '02', '03'];
 */
 const nameOptions = [
   {
@@ -23,15 +24,33 @@ const nameOptions = [
   },
 ];
 
+const currentYear = new Date().getFullYear();
+
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
+
 const CreateAcademicSemester = () => {
   const onsubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    // console.log("data", Number(data.name) - 1);
+    const name = nameOptions[Number(data?.name) - 1]?.label;
+    const code = nameOptions[Number(data?.name) - 1]?.value;
+    const year = data?.year;
+
+    const semesterData = {
+      name,
+      code,
+      year,
+    };
+    console.log(semesterData);
   };
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onsubmit}>
           <PHSelect label="name" name="name" options={nameOptions}></PHSelect>
+          <PHSelect label="year" name="year" options={yearOptions}></PHSelect>
           <Button htmlType="submit">submit</Button>
         </PHForm>
       </Col>
