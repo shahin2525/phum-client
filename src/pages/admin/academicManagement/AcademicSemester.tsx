0ba93d41c8d2f2990c6a9e8837ff1ccc25a6f593
shahin2525 <shahin451999@gmail.com>
2025-01-22 19:39:50 -0800

    add year filter

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,63 +1,70 @@
 import { Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllSemesterQuery } from "../../../redux/feature/admin/academicManagement.api";
 import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { useState } from "react";
 export type TTableData = Pick<
   TAcademicSemester,
   "_id" | "name" | "year" | "startMonth" | "endMonth"
 >;
 const AcademicSemester = () => {
-  const { data: semesterData } = useGetAllSemesterQuery([
-    { name: "year", value: "2025" },
-  ]);
-  console.log("academic semester", semesterData);
+  const [param, setParam] = useState([]);
+  const { data: semesterData } = useGetAllSemesterQuery(param);
 
   const columns: TableColumnsType<TTableData> = [
     {
       title: "Name",
       dataIndex: "name",
+      key: "name",
 
       filters: [
         {
-          text: "Joe",
-          value: "Joe",
+          text: "Autumn",
+          value: "Autumn",
         },
         {
-          text: "Jim",
-          value: "Jim",
+          text: "Fall",
+          value: "Fall",
         },
         {
-          text: "Submenu",
-          value: "Submenu",
-          children: [
-            {
-              text: "Green",
-              value: "Green",
-            },
-            {
-              text: "Black",
-              value: "Black",
-            },
-          ],
+          text: "Summer",
+          value: "Summer",
         },
       ],
     },
     {
       title: "Year",
       dataIndex: "year",
+      key: "year",
+      filters: [
+        {
+          text: "2024",
+          value: "2024",
+        },
+        {
+          text: "2025",
+          value: "2025",
+        },
+        {
+          text: "2026",
+          value: "2026",
+        },
+      ],
     },
     {
       title: "StartMonth",
       dataIndex: "startMonth",
+      key: "startMonth",
     },
     {
       title: "EndMonth",
       dataIndex: "endMonth",
+      key: "endMonth",
     },
   ];
 
   const tableData = semesterData?.data!.map(
     ({ _id, name, year, startMonth, endMonth }) => ({
-      _id,
+      key: _id,
       name,
       year,
       startMonth,
@@ -71,7 +78,17 @@ const AcademicSemester = () => {
     sorter,
     extra
   ) => {
-    console.log("params", pagination, filters, sorter, extra);
+    const queryParams = [];
+    if (extra.action === "filter") {
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: "year", value: item })
+      );
+    }
+
+    setParam(queryParams);
   };
 
   return (
--- src/redux/feature/admin/academicManagement.api.ts
+++ src/redux/feature/admin/academicManagement.api.ts
@@ -8,7 +8,11 @@ const academicManagementApi = baseApi.injectEndpoints({
     getAllSemester: builder.query({
       query: (args) => {
         const params = new URLSearchParams();
-        params.append(args[0].name, args[0].value);
+        if (args) {
+          args.forEach((item) => {
+            params.append(item.name, item.value);
+          });
+        }
 
         return {
           url: "/academic-semesters",
@@ -17,7 +21,7 @@ const academicManagementApi = baseApi.injectEndpoints({
         };
       },
       transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
-        console.log("inside redux", response);
+        // console.log("inside redux", response);
 
         return {
           data: response?.data,
